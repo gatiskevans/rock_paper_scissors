d829f4ae96bb87e108e543f87b9d8e5bb4634e9b
Gatis Ķevans <gatis.kevans@gmail.com>
2021-08-25 10:28:22 +0300

    Rock Paper Scissors Game

---
+++ rock-paper-scissors.php
@@ -0,0 +1,32 @@
+<?php
+
+    $options = ["Rock", "Paper", "Scissors"];
+    $computer = $options[rand(0, 2)];
+
+    foreach ($options as $index => $option) {
+        echo "$index | $option\n";
+    }
+
+    $selection = readline("Pick your option: ");
+    $selection = $options[$selection];
+
+    function findWinner(string $selection, string $computer): string {
+        switch(true){
+            case $selection === $computer:
+                return "It's a tie";
+            case $selection === "Rock" && $computer === "Paper" ||
+                 $selection === "Paper" && $computer === "Scissors" ||
+                 $selection === "Scissors" && $computer === "Rock":
+                return "Computer wins!";
+            case $selection === "Rock" && $computer === "Scissors" ||
+                 $selection === "Paper" && $computer === "Rock" ||
+                 $selection === "Scissors" && $computer === "Paper":
+                return "You win!";
+        }
+        return "Something went wrong!";
+    }
+
+    echo "You chose: $selection\n";
+    echo "Computer chose: $computer\n";
+
+    echo findWinner($selection, $computer);
